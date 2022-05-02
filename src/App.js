7fd15d1fb2eef276f5b3bbbf0cb50a8cc25212e8
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-05-02 16:11:21 -0400

    Create Pet component

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// imaginary code here
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt me pls"),
+    React.createElement(Pet, {
+        name: "Calci",
+        animal: "Cat",
+        breed: "Surprise"
+    }),
+    React.createElement(Pet, {
+        name: "Ceniza",
+        animal: "Cat",
+        breed: "Surprise JR."
+    }),
+    React.createElement(Pet, {
+        name: "Luna",
+        animal: "Dog",
+        breed: "Havanese"
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
