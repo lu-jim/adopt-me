071d15bc26f69129533379a49ded0b88134c07be
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-05-02 18:40:23 -0400

    Create Pet component and update browserslist

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Pet from './Pet';
 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
 
 const App = () => {
   return React.createElement("div", {}, [
