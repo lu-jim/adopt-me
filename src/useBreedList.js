80b6d42a4117b050403877707e3bc348742a9341
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-05-03 15:52:43 -0400

    Create custom useBreedList hook

---
+++ src/useBreedList.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+const localCache = {}; // We could use localStorage instead
+
+export default function useBreedList(animal) {
+  const [breedList, setBreedList] = useState([]);
+  const [status, setStatus] = useState("unloaded");
+
+  useEffect(() => {
+    if (!animal) {
+      setBreedList([]);
+    } else if (localCache[animal]) {
+      setBreedList(localCache[animal]);
+    } else {
+      requestBreedList();
+    }
+
+    const requestBreedList = async () => {
+      setBreedList([]);
+      setStatus("loading");
+
+      const res = await fetch(
+        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+      );
+      const json = await res.json
+      localCache[animal] = json.breeds || [];
+      setBreedList(localCache[animal]);
+      setStatus("loaded");
+    }
+  }, [animal]);
+
+  return [breedList, status];
+}
