f24a201ec312a2cb6716b054c40594d24685732c
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-05-03 09:30:38 -0400

    Use jsx and update linters

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import React from 'react';
 import { render } from 'react-dom';
 import Pet from './Pet';
 
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt me pls"),
-    React.createElement(Pet, {
-      name: "Calci",
-      animal: "Cat",
-      breed: "Surprise",
-    }),
-    React.createElement(Pet, {
-      name: "Ceniza",
-      animal: "Cat",
-      breed: "Surprise JR.",
-    }),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-  ]);
+  return(
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Calci" animal="Cat" breed="Surprise"></Pet>
+      <Pet name="Ceniza" animal="Cat" breed="Surprise"></Pet>
+      <Pet name="Luna" animal="Dog" breed="Havanese"></Pet>
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
